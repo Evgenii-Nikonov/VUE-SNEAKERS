bbe17342c49c4a93d450857918da8912905bab0e
Evgenii Nikonov <naterid@yandex.ru>
2025-08-03 23:05:37 +0700

    Добавлены файлы проекта, реализована вёрстка шапки в соответствии с дизайном

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,31 @@ import MyComponent from './components/MyComponent.vue'
 </script>
 
 <template>
-  <div class="text-3x1 font-bold underline bg-green">
-    <MyComponent title="adidas must only" :price="14999" :isAdded="false" />
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-15">
+    <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+      <div class="flex items-center gap-4">
+        <img class="w-10" src="/logo.png" alt="" />
+        <div class="">
+          <h1 class="text-xl font-bold uppercase">vue sneakers</h1>
+          <p class="text-slate-400">Магазин лучших кросовок</p>
+        </div>
+      </div>
+
+      <ul class="flex items-center gap-10 text-gray-500">
+        <li class="flex items-center gap-3 hover:text-black cursor-pointer duration-150 ease-in">
+          <img src="/cart.svg" alt="" />
+          <span class="font-bold">1205 руб.</span>
+        </li>
+        <li class="flex items-center gap-3 hover:text-black cursor-pointer duration-150 ease-in">
+          <img src="/heart.svg" alt="" />
+          <span>Закладки</span>
+        </li>
+        <li class="flex items-center gap-3 hover:text-black cursor-pointer duration-150 ease-in">
+          <img src="/profile.svg" alt="" />
+          <span>Профиль</span>
+        </li>
+      </ul>
+    </header>
   </div>
 </template>
 
