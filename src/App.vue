752befed7056271b589d91e4f5932a56bf162987
Evgenii Nikonov <naterid@yandex.ru>
2025-08-03 21:00:08 +0700

    создан проект, подключен tailwind css

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MyComponent from './components/MyComponent.vue'
+</script>
+
+<template>
+  <div class="text-3x1 font-bold underline bg-green">
+    <MyComponent title="adidas must only" :price="14999" :isAdded="false" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean,
+})
+</script>
+
+<template>
+  <div class="product">
+    <h1>Товар: {{ title }}</h1>
+    <h2>цена: {{ price }}</h2>
+    <h3>Добавлен: {{ isAdded }}</h3>
+
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.product {
+  border: 1px solid teal;
+  padding: 12px;
+  /* margin-bottom: 10px; */
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  width: 20%;
+}
+</style>
