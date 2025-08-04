a189c1796a6f6c02d1314245de162ff8bc10c50d
Evgenii Nikonov <naterid@yandex.ru>
2025-08-04 23:15:48 +0700

    реализован компонент карточки товара, а также компонент списка карточек товаров

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  imageUrl: String,
+  title: String,
+  price: Number,
+})
+</script>
+
+<template>
+  <div
+    class="bg-white relative border border-slate-300 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition"
+  >
+    <img src="/like-2.svg" alt="like" class="absolute top-8 left-8" />
+    <img :src="imageUrl" alt="sneaker" />
+    <p class="mt-2">{{ title }}</p>
+
+    <div class="flex justify-betwee mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>{{ price }} руб.</b>
+      </div>
+      <img src="/plus.svg" alt="plus" />
+    </div>
+  </div>
+</template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Card from './Card.vue'
+
+const props = defineProps()
+</script>
+
+<template>
+  <div class="cards grid grid-cols-4 gap-5">
+    <Card
+      imageUrl="/sneakers/sneakers-1.jpg"
+      title="Мужские кросовки Nike Blazer mid"
+      :price="5000"
+    />
+    <Card />
+  </div>
+</template>
