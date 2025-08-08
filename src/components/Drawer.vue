1bce7541e8d4806234fc5f2b4646ad4490d3b624
Evgenii Nikonov <naterid@yandex.ru>
2025-08-08 21:25:54 +0700

    Скорректирована вёрстка корзины, добавлена кнопка офрмления заказа, общая сумма, сумма налога

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,14 +1,31 @@
 <script setup>
 import DrawerHead from './DrawerHead.vue'
-import CartItem from './CartItem.vue'
+import CartItemList from './CartItemList.vue'
 </script>
 
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-50"></div>
-  <div class="bg-white w-100 h-full fixed right-0 top-0 z-20 p-8">
+  <div class="flex flex-col bg-white w-100 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead />
-    <CartItem />
-    <CartItem />
-    <CartItem />
+    <CartItemList />
+
+    <div class="flex flex-col gap-4 mb-5 my-7">
+      <div class="flex gap-2">
+        <span>Итого:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>12900 ₽</b>
+      </div>
+      <div class="flex gap-2">
+        <span>Налог 5%:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>900 ₽</b>
+      </div>
+
+      <button
+        class="mt-4 bg-lime-500 rounded-xl w-full py-3 disabled:bg-slate-300 disabled:cursor-none text-white hover:bg-lime-600 transition active:bg-lime-700 cursor-pointer"
+      >
+        Оформить заказ
+      </button>
+    </div>
   </div>
 </template>
