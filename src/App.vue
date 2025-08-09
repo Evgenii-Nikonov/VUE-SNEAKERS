e948772c55d12900e39dbef9ea5f49ec7af324f8
Evgenii Nikonov <naterid@yandex.ru>
2025-08-09 18:26:12 +0700

    Реализован функционал сортировкки кроссовок, а также написан скрипт для реализации поиска на стороне фронтенда

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { watch, computed, onMounted, ref } from 'vue'
 
 import axios from 'axios'
 import Header from './components/Header.vue'
@@ -7,9 +7,37 @@ import CardList from './components/CardList.vue'
 
 const items = ref([])
 
+const sortBy = ref('')
+const searchQuery = ref('')
+
+const onChangeSelect = (event) => {
+  console.log(event.target.value)
+  sortBy.value = event.target.value
+}
+
 onMounted(async () => {
   try {
     const { data } = await axios.get('https://0d2e8a6fb9e1c979.mokky.dev/items')
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+// Реализация поиска по товарам на стороне фронтенда
+// const filteredItems = computed(() => {
+//   return items.value.filter(
+//     (item) => item.title && item.title.toLowerCase().includes(searchQuery.value.toLowerCase()),
+//   )
+// })
+
+//*При изменении sortby делается запрос к серверу в который будет передаваться 'sortby' + sortby
+
+watch(sortBy, async () => {
+  try {
+    const { data } = await axios.get(
+      'https://0d2e8a6fb9e1c979.mokky.dev/items?sortBy=' + sortBy.value,
+    )
     items.value = data
   } catch (err) {
     console.log(err)
@@ -26,16 +54,22 @@ onMounted(async () => {
       <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
         <div class="flex gap-4">
-          <select vlass="border [y-2 px-3 rounded-md outline-none cursor-pointer" name="" id="">
-            <option value="">По названию</option>
-            <option value="">По цене (дешёвые)</option>
-            <option value="">По цене (дорогие)</option>
+          <select
+            @change="onChangeSelect"
+            vlass="border [y-2 px-3 rounded-md outline-none cursor-pointer"
+            name=""
+            id=""
+          >
+            <option value="namr">По названию</option>
+            <option value="price">По цене (дешёвые)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
         </div>
 
         <div class="relative">
           <img class="absolute left-4 top-3" src="/search.svg" alt="" />
           <input
+            v-model="searchQuery"
             class="border rounded-md py-2 pl-12 pr-4 outline-none focus:border-gray-400"
             type="text"
             placeholder="Поиск..."
